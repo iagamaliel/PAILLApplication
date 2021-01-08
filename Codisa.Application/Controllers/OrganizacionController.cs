f9d820f040563162fdbaf78ea2954a61a73fb681
iagamaliel <caleb1693@hotmail.com>
2021-01-08 11:01:18 -0600

    versionando aplicacion web PAILL

---
+++ Codisa.Application/Controllers/OrganizacionController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Codisa.Infrastructure.ParamsDTO;
+using Codisa.Infrastructure.Repository.InterfacesRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codisa.Application.Controllers
+{
+    public class OrganizacionController : Controller
+    {
+        #region ATTRIBUTES
+        private readonly IEmpleadoRepository _empleadoRepository;
+        private readonly IMapper _mapper;
+        #endregion
+
+        #region CONSTRUCTOR
+        public OrganizacionController (IMapper mapper, IEmpleadoRepository empleadoRepository)
+        {
+            _empleadoRepository = empleadoRepository ?? throw new ArgumentNullException(nameof(empleadoRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+        #endregion
+
+        #region "METODOS"
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IList<EmpleadoDto> empleadoDtos = _mapper.Map<IList<EmpleadoDto>>(await _empleadoRepository.GetAllEmployee());
+            ViewData["EmpleadosA"] = empleadoDtos.Where(e => e.IdJefe == 1);
+            ViewData["EmpleadosB"] = empleadoDtos.Where(e => e.IdJefe == 2);
+
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ Codisa.Application/Models/Empleado/UpdateEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Codisa.Application.Models.Empleado
+{
+    public class UpdateEmployee
+    {
+        [Required]
+        public int IdEmpleado { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MinLength(3), MaxLength(100)]
+        public string NombreCompleto { get; set; }
+        [MaxLength(50)]
+        public string Cedula { get; set; }
+        [EmailAddress(ErrorMessage = "Formato de correo incorrecto")]
+        [MaxLength(100)]
+        public string Correo { get; set; }
+        [Required(ErrorMessage = "La fecha de Nacmiento es requerida")]
+        public DateTime FechaNacimiento { get; set; }
+        [Required(ErrorMessage = "La fecha de ingreso es requerida")]
+        public DateTime FechaIngreso { get; set; }
+        public int IdJefe { get; set; }
+        [Required(ErrorMessage = "La area es requerida")]
+        public int IdArea { get; set; }
+    }
+}
